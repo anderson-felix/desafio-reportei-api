c6de7cd68a5144449871abb900a33930ea978679
Anderson Felix <andersonfelixmsouza@gmail.com>
2020-12-13 15:43:45 -0300

    create mongoose connection app

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,16 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-
 import routes from './routes';
+import server from './server';
 
 class App {
-  server = express(); //quando a classe for chamada, vai chamar automaticamante as funçoes middlewares e routes
   constructor() {
-    mongoose.connect(
-      'mongodb+srv://<username>:<password>@desafio-reportei.zpzhw.mongodb.net/<dbname>?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    this.middlewares();
     this.routes();
+    this.server();
   }
-  middlewares() {
-    this.server.use(cors());
-
-    this.server.use(
-      '/files',
-      express.static(path.resolve(__dirname, '..', 'uploads'))
-    );
-
-    this.server.use(express.json());
+  server() {
+    server;
   }
   routes() {
-    this.server.use(routes);
+    routes;
   }
 }
-export default new App().server;
+export default new App();
