058e613993df15e5db649c3bcf99cb91fb3d5fca
Anderson Felix <andersonfelixmsouza@gmail.com>
2020-12-13 15:44:18 -0300

    Update UserModel.ts

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,13 +1,49 @@
-import { Schema, model } from 'mongoose';
+import { prop, modelOptions, getModelForClass } from '@typegoose/typegoose';
 
-const schema = new Schema({
-  user_name: String,
-  full_name: String,
-  biography: String,
-  is_private: Boolean,
-  has_channel: Boolean,
-  is_business_account: Boolean,
-  business_category_name: String,
-});
+import { db } from '../utils/db';
 
-export default model('UserModel', schema);
+@modelOptions({ schemaOptions: { collection: 'users' } })
+export class User {
+  @prop({ required: true, unique:true, type: String })
+  username: string;
+
+  @prop({ required: true, type:String })
+  profile_pic_url: string;
+
+  @prop({ required: true, type:String })
+  full_name: string;
+
+  @prop({type:String})
+  biography: string;
+
+  @prop({type:String})
+  external_url: string;
+
+  @prop({ required: true, type:Boolean })
+  is_joined_recently: boolean;
+
+  @prop({ required: true, type:Boolean })
+  is_private: boolean;
+
+  @prop({ required: true, type:Boolean })
+  has_channel: boolean;
+
+  @prop({ required: true, type:Boolean })
+  is_business_account: boolean;
+
+  @prop({type:String})
+  business_category_name: string;
+
+  @prop({ required: true, min: 0, type:Number })
+  follow: number;
+
+  @prop({ required: true, min: 0, type:Number })
+  followed_by: number;
+
+  @prop({ required: true, min: 0, type:Number })
+  timeline_media_count: number;
+}
+
+export const userModel = getModelForClass(User);
+
+userModel.db = db;
