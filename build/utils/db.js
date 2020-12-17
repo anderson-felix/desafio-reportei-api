8f00f26924b3a186c398698f7620a42689f28823
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2020-12-16 22:42:00 -0300

    Lint ajust

--- build/utils/db.js
+++ build/utils/db.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+    return (mod && mod.__esModule) ? mod : { 'default': mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.db = void 0;
-const mongoose_1 = __importDefault(require("mongoose"));
+const mongoose_1 = __importDefault(require('mongoose'));
 mongoose_1.default.connect('mongodb+srv://instagram_access:desafioreportei777@desafio-reportei.zpzhw.mongodb.net/instool?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
