265d57479bc6dc9604eb9ef7802b7fcec0fd2ca3
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2020-12-16 22:41:32 -0300

    Lint ajust

--- build/controllers/InstagramSearch.js
+++ build/controllers/InstagramSearch.js
@@ -1,9 +1,9 @@
-"use strict";
+'use strict';
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+    return (mod && mod.__esModule) ? mod : { 'default': mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
+Object.defineProperty(exports, '__esModule', { value: true });
+const axios_1 = __importDefault(require('axios'));
 class InstagramSearch {
     async search(req, res) {
         const { username } = req.params;
