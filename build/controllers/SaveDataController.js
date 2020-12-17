b24c5338ae4722629eae6d26656ae2dbf42f7812
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2020-12-16 22:41:41 -0300

    Lint ajust

--- build/controllers/SaveDataController.js
+++ build/controllers/SaveDataController.js
@@ -1,6 +1,6 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const UserModel_1 = require("../models/UserModel");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const UserModel_1 = require('../models/UserModel');
 class SaveDataController {
     async store(req, res) {
         const { userData } = req.body;
