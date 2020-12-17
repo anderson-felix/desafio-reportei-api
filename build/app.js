c2eb6fae97c0e741aac733317be94d18e6229fbc
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2020-12-16 22:41:25 -0300

    Lint ajust

--- build/app.js
+++ build/app.js
@@ -1,10 +1,10 @@
-"use strict";
+'use strict';
 var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+    return (mod && mod.__esModule) ? mod : { 'default': mod };
 };
-Object.defineProperty(exports, "__esModule", { value: true });
-const routes_1 = __importDefault(require("./routes"));
-const server_1 = __importDefault(require("./server"));
+Object.defineProperty(exports, '__esModule', { value: true });
+const routes_1 = __importDefault(require('./routes'));
+const server_1 = __importDefault(require('./server'));
 class App {
     constructor() {
         this.routes();
