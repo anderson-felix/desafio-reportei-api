fb5d965dac9b84a32d8e9f93b7848e5e34180931
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2020-12-16 22:41:55 -0300

    Lint ajust

--- build/routes.js
+++ build/routes.js
@@ -1,5 +1,5 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
+const express_1 = require('express');
 const routes = express_1.Router();
 exports.default = routes;
