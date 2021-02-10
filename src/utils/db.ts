acb4ab1d3bf331b9629247d5524e50bd4e5eab90
Anderson Felix <63384863+anderson-felix@users.noreply.github.com>
2021-02-10 20:38:19 -0300

    update db connection

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 mongoose.connect(
-  'mongodb+srv://instagram_access:desafioreportei777@desafio-reportei.zpzhw.mongodb.net/instool?retryWrites=true&w=majority',
+  'mongodb+srv://instagram_access:ppOySIhPdJo2JHm8@dark-labs.zpzhw.mongodb.net/instool?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
 );
-
+console.log('MONGO [OK]');
 export const db = mongoose.connection;
